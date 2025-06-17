4ff923d49272bbacf91fb83759e29f61edd71799
pentahead <dhiyaafaruq@gmail.com>
2025-06-18 03:21:18 +0700

    env

--- src/services/api.js
+++ src/services/api.js
@@ -1,62 +1,70 @@
-import axios from 'axios';
+import axios from "axios";
 
-const userServiceUrl = 'http://localhost:3001';
-const todoServiceUrl = 'http://localhost:3002';
+const userServiceUrl =
+  process.env.REACT_APP_USER_SERVICE_URL || "http://localhost:3001";
+const todoServiceUrl =
+  process.env.REACT_APP_TODO_SERVICE_URL || "http://localhost:3002";
 
 // Setup axios interceptor for JWT
 const setupAuthInterceptor = (token) => {
   axios.interceptors.request.use(
-    config => {
+    (config) => {
       if (token) {
         config.headers.Authorization = `Bearer ${token}`;
       }
       return config;
     },
-    error => Promise.reject(error)
+    (error) => Promise.reject(error)
   );
 };
 
 // User service API
 export const userApi = {
   login: (email, password) => {
-    return axios.post(`${userServiceUrl}/login`, { email, password })
-      .then(response => {
+    return axios
+      .post(`${userServiceUrl}/login`, { email, password })
+      .then((response) => {
         const { token } = response.data;
         setupAuthInterceptor(token);
         return response.data;
       });
   },
-  
+
   register: (userData) => {
-    return axios.post(`${userServiceUrl}/register`, userData)
-      .then(response => {
+    return axios
+      .post(`${userServiceUrl}/register`, userData)
+      .then((response) => {
         const { token } = response.data;
         setupAuthInterceptor(token);
         return response.data;
       });
   },
-  
+
   getUser: (userId) => {
-    return axios.get(`${userServiceUrl}/users/${userId}`)
-      .then(response => response.data);
+    return axios
+      .get(`${userServiceUrl}/users/${userId}`)
+      .then((response) => response.data);
   },
-  
+
   verifyToken: (token) => {
     setupAuthInterceptor(token);
-    return axios.post(`${userServiceUrl}/verify-token`, { token })
-      .then(response => response.data);
-  }
+    return axios
+      .post(`${userServiceUrl}/verify-token`, { token })
+      .then((response) => response.data);
+  },
 };
 
 // Todo service API
 export const todoApi = {
   getTodos: () => {
-    return axios.get(`${todoServiceUrl}/todos`)
-      .then(response => response.data);
+    return axios
+      .get(`${todoServiceUrl}/todos`)
+      .then((response) => response.data);
   },
-  
+
   createTodo: (todoData) => {
-    return axios.post(`${todoServiceUrl}/todos`, todoData)
-      .then(response => response.data);
-  }
-}; 
+    return axios
+      .post(`${todoServiceUrl}/todos`, todoData)
+      .then((response) => response.data);
+  },
+};
